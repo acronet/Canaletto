b749cb98666d15e21b21ffbc26d4cb1551031a62
ACRONET Paradigm <acronet@acronet.cc>
2014-06-05 17:55:20 +0200

    First Commit

---
+++ AcroboardStation_v01_LaSpezia/laspezia/Debug/makedep.mk
@@ -0,0 +1,10 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+laspezia.c
+
+services\config\config.c
+
+services\datalogger\datalogger.c
+
